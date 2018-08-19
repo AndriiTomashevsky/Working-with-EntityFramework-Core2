6eb4fc8d1b9ba1ec0a8632101534150b771c1ed9
AndriiTomashevsky <tomashevskiy.a@gmail.com>
2018-08-19 14:46:09 +0300

    11 - Enhancing the Model

---
+++ README.txt
@@ -0,0 +1,1 @@
+Projects\EntityFramework Core\Freeman A. - Pro ASP.NET Core MVC 2, Seventh Edition - 2017\08 - SportsStore\SportsStore\
--- SportsStore/Models/Order.cs
+++ SportsStore/Models/Order.cs
@@ -6,6 +6,7 @@ namespace SportsStore.Models
     {
         public int OrderID { get; set; }
         public ICollection<CartLine> Lines { get; set; }
+        public bool Shipped { get; set; }
         public string Name { get; set; }
         public string Line1 { get; set; }
         public string Line2 { get; set; }
